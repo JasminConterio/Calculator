afe1efab68077771408ed2763f5e189f8c2531d9
unknown <jasmin.chana29@gmail.com>
2021-07-01 21:30:32 +0100

    Added a single multiplier

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
+
+// Get 2 numbers from the user, convert to float, multiply and pring
+console.log('Enter a number to multiply:');
+// const response = readline.prompt(); 
+// Instructions suggest using a const, but this way we don't need separate variables for each user input
+var response = readline.prompt();
+num1 = parseFloat(response);
+
+console.log('Enter a second number to multiply:');
+response = readline.prompt();
+num2 = parseFloat(response);
+
+answer = num1*num2;
+
+console.log('The product of you numbers is ' + answer)
