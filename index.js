1fa731736863ded34cb0df5546effa35ce2d515c
unknown <jasmin.chana29@gmail.com>
2021-09-03 21:55:27 +0100

    Refactored code into functions

--- index.js
+++ index.js
@@ -1,58 +1,103 @@
 const readline = require('readline-sync');
 
-// ===========================================
-// Get instructions from user
+printWelcomeMessage();
 
-console.log('Welcome to the calculator!');
-console.log('===========================');
+// continue calculations until the using wants to calculate 0 arguments
+do
+    performOneCalculation();
+while(n_args)   
 
-console.log('Please enter the operator (+ - * /)');
-const operator = readline.prompt();
 
-console.log('How many numbers to you want to ' + operator + '?');
-const n_numbers = readline.prompt();
 
-var numbers = Array(n_numbers);
 
-for(let i = 0; i<n_numbers; i++){
-    console.log('Please enter number ' + (i+1));
-    let response = readline.prompt();
-    numbers[i] = parseFloat(response);
+function printWelcomeMessage(){
+    console.log('Welcome to the calculator!');
+    console.log('===========================');
 }
 
 
-// ===========================================
-// Run calculator
+function performOneCalculation(){
+    // get calculation instructions
+    operator = inputOperator();
+    n_args = inputNargs();
 
+    if (n_args){
+        args = inputArgs();
+        // run calculation
+        var answer = runCalculation();
+        //output answer
+        console.log('The answer is ' + answer);
+    }
 
-
-
-var answer = numbers[0];
-
-switch (operator) {
-    case '+':
-        for(i = 1; i<n_numbers; i++){
-            answer = answer + numbers[i];
-        }           
-        break;
-    case '-':
-        for(i = 1; i<n_numbers; i++){
-            answer = answer - numbers[i];
-        }  
-        break;
-    case '*':
-        for(i = 1; i<n_numbers; i++){
-            answer = answer * numbers[i];
-        }  
-        break;
-    case '/':
-        for(i = 1; i<n_numbers; i++){
-            answer = answer / numbers[i];
-        }  
-        break;
-
+    return n_args;
 }
 
 
+function inputOperator(){
+    console.log('Please enter the operator (+ - * /)');
+    const op = readline.prompt();
 
-console.log('The answer is ' + answer);
+    return op;
+}
+
+function inputNargs(){
+    console.log('How many arguments to you want to ' + operator + '?');
+    const n_numbers = inputNumber();
+
+    return n_numbers;
+}
+
+function inputArgs(){
+    var numbers = Array(n_args);
+
+    for(let i = 0; i<n_args; i++){
+        numbers[i] = inputNumber();
+    }
+
+    return numbers;
+}
+
+function inputNumber(){
+    let number;
+    do {
+        console.log('Please enter a number:');
+        const response = readline.prompt();
+        number = +response;
+        if(isNaN(number)){
+            console.log('Invalid number!');
+        }
+    }
+    while(isNaN(number))
+    
+
+    return number;
+    
+
+    //const number = parseFloat(response);
+
+    
+}
+
+function runCalculation(){
+    var ans = args[0];
+
+    for(i=1; i<n_args; i++){
+        switch (operator) {
+            case '+':
+                ans = ans + args[i];
+                break;
+            case '-':
+                ans = ans - args[i];
+                break;
+            case '*':
+                ans = ans*args[i];
+                break;
+            case '/':
+                ans = ans/args[i];
+
+        }
+    }
+
+    return ans
+}
+
