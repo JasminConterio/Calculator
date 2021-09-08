dfa59ffe57f5cfd60c86445d5e97706849e20007
unknown <jasmin.chana29@gmail.com>
2021-09-08 22:27:45 +0100

    Re-organised, improved messaging, check valid operator

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const readline = require('readline-sync');
 
-printWelcomeMessage();
-
-// continue calculations until the using wants to calculate 0 arguments
-do
-    performOneCalculation();
-while(n_args)   
-
-
-
 
 function printWelcomeMessage(){
     console.log('Welcome to the calculator!');
@@ -16,71 +7,46 @@ function printWelcomeMessage(){
 }
 
 
-function performOneCalculation(){
-    // get calculation instructions
-    operator = inputOperator();
-    n_args = inputNargs();
+function getStringInputWithPrompt(prompt){
+    console.log(prompt);
+    const response = readline.prompt();
+    return response;
+}
 
-    if (n_args){
-        args = inputArgs();
-        // run calculation
-        var answer = runCalculation();
-        //output answer
-        console.log('The answer is ' + answer);
+function getNumberInputWithPrompt(prompt){
+    let response;
+    do {
+        console.log(prompt);
+        const inputStr = readline.prompt();
+        response = +inputStr;
+        if(isNaN(response)){
+            console.log('Invalid number!');
+        }
     }
-
-    return n_args;
-}
-
-
-function inputOperator(){
-    console.log('Please enter the operator (+ - * /)');
-    const op = readline.prompt();
-
-    return op;
-}
-
-function inputNargs(){
-    console.log('How many arguments to you want to ' + operator + '?');
-    const n_numbers = inputNumber();
-
-    return n_numbers;
+    while(isNaN(response))
+    return response;
 }
 
 function inputArgs(){
     var numbers = Array(n_args);
-
     for(let i = 0; i<n_args; i++){
-        numbers[i] = inputNumber();
+        numbers[i] = getNumberInputWithPrompt(`Please enter number ${i+1}:`);
     }
-
     return numbers;
 }
 
-function inputNumber(){
-    let number;
-    do {
-        console.log('Please enter a number:');
-        const response = readline.prompt();
-        number = +response;
-        if(isNaN(number)){
-            console.log('Invalid number!');
-        }
+function getOperator(){
+    operator = getStringInputWithPrompt('Please enter the operator (+ - * /)');
+    const validOperators = ['+','-','*','/'];
+    if (!(validOperators.includes(operator))){
+        console.log('Invalid operator! Please try again.')
+        return 0;
     }
-    while(isNaN(number))
-    
-
-    return number;
-    
-
-    //const number = parseFloat(response);
-
-    
+    return operator;
 }
 
 function runCalculation(){
     var ans = args[0];
-
     for(i=1; i<n_args; i++){
         switch (operator) {
             case '+':
@@ -94,10 +60,33 @@ function runCalculation(){
                 break;
             case '/':
                 ans = ans/args[i];
-
         }
     }
-
-    return ans
+    return ans;
 }
 
+function performOneCalculation(){
+    // get operator, check it is valid  
+    do 
+        operator = getOperator();
+    while(!(operator))
+    // get arguments, check they are valid, run calculation, print answer
+    n_args = getNumberInputWithPrompt(`How many arguments to you want to ${operator}? (0 to quit)`);
+    if (n_args){
+        args = inputArgs();
+        var answer = runCalculation();
+        console.log('The answer is ' + answer);
+    }
+    return n_args;
+}
+
+
+// =====================================================================
+// Main code
+// =====================================================================
+
+printWelcomeMessage();
+// continue calculations until the using wants to calculate 0 arguments
+do
+    performOneCalculation();
+while(n_args) 
